09c8b915a75068de5aba0421bf61a455d4963d97
Elizaveta Zmievskaya <zmievskaya.liza@mail.ru>
2016-11-18 11:38:06 +0200

    Migrations fixed, Rbac configured

--- commands/RbacController.php
+++ commands/RbacController.php
@@ -8,8 +8,20 @@
 
 namespace app\commands;
 
+use Yii;
+use yii\console\Controller;
 
-class RbacController
+class RbacController extends Controller
 {
+    public function actionInit()
+    {
+        $auth = Yii::$app->authManager;
 
+        $user = $auth->createRole('user');
+        $auth->add($user);
+
+        $admin = $auth->createRole('admin');
+        $auth->add($admin);
+        $auth->addChild($admin, $user);
+    }
 }
