08e04769d5918c014abf3bbd4da2c7ea38c44636
Elizaveta Zmievskaya <zmievskaya.liza@mail.ru>
2016-11-23 15:17:04 +0200

    Migration for hashes modified

--- migrations/m161118_165532_create_hashes_table.php
+++ migrations/m161118_165532_create_hashes_table.php
@@ -5,19 +5,21 @@ use yii\db\Migration;
 /**
  * Handles the creation of table `hash`.
  */
-class m161118_165532_create_hash_table extends Migration
+class m161118_165532_create_hashes_table extends Migration
 {
     /**
      * @inheritdoc
      */
     public function up()
     {
-        $this->createTable('hash', [
+        $this->createTable('hashes', [
             'id' => $this->primaryKey(),
-            'user_id' => $this->integer()->unique(),
+            'user_id' => $this->integer(),
             'hash' => $this->string(),
             'type' => "ENUM('register','recover') DEFAULT 'register'"
         ]);
+
+        $this->addForeignKey('fk_user_id', 'hashes', 'user_id', 'users', 'id', 'cascade', 'cascade');
     }
 
     /**
@@ -25,6 +27,6 @@ class m161118_165532_create_hash_table extends Migration
      */
     public function down()
     {
-        $this->dropTable('hash');
+        $this->dropTable('hashes');
     }
 }
