8c96acafaf4bd1b6dc3b9efa8fbb17844f7f3be9
Elizaveta Zmievskaya <zmievskaya.liza@mail.ru>
2016-11-30 15:09:37 +0200

    Test config and db dump modified.

--- tests/_data/dump.sql
+++ tests/_data/dump.sql
@@ -157,13 +157,13 @@ LOCK TABLES `hashes` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `mail_template`
+-- Table structure for table `mail_templates`
 --
 
-DROP TABLE IF EXISTS `mail_template`;
+DROP TABLE IF EXISTS `mail_templates`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `mail_template` (
+CREATE TABLE `mail_templates` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `key` varchar(255) DEFAULT NULL,
   `body` text,
@@ -176,12 +176,13 @@ CREATE TABLE `mail_template` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `mail_template`
+-- Dumping data for table `mail_templates`
 --
 
-LOCK TABLES `mail_template` WRITE;
-/*!40000 ALTER TABLE `mail_template` DISABLE KEYS */;
-/*!40000 ALTER TABLE `mail_template` ENABLE KEYS */;
+LOCK TABLES `mail_templates` WRITE;
+/*!40000 ALTER TABLE `mail_templates` DISABLE KEYS */;
+INSERT INTO `mail_templates` VALUES (1,'REGISTER_CONFIRM','<p>Hello, {{name}}!</p><p>Thank you for registration! To activate your account, click here please {{link}}.</p><p>After confirmation you will be automatically logged in.</p>','Confirm registration','2016-11-28 11:41:20','Registration confirmation');
+/*!40000 ALTER TABLE `mail_templates` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -204,7 +205,7 @@ CREATE TABLE `migration` (
 
 LOCK TABLES `migration` WRITE;
 /*!40000 ALTER TABLE `migration` DISABLE KEYS */;
-INSERT INTO `migration` VALUES ('m000000_000000_base',1479461437),('m140506_102106_rbac_init',1479461437),('m161114_123235_create_mail_template_table',1479909417),('m161118_100631_create_users_table',1479909417),('m161118_100646_assign_admin_role',1479909417),('m161118_165532_create_hashes_table',1479909418),('m161122_093935_create_admin_user',1479909418),('m161123_092314_add_fk_user_assignment',1479909418);
+INSERT INTO `migration` VALUES ('m000000_000000_base',1480327197),('m140506_102106_rbac_init',1480327197),('m161114_123235_create_mail_templates_table',1480327198),('m161118_100631_create_users_table',1480327198),('m161118_100633_add_fk_user_assignment',1480327198),('m161118_100634_create_admin_user',1480327199),('m161118_100646_assign_admin_role',1480327199),('m161118_165532_create_hashes_table',1480327199),('m161123_152129_add_register_confirm_template',1480333280),('m161130_094719_make_email_unique',1480499621);
 /*!40000 ALTER TABLE `migration` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -226,7 +227,8 @@ CREATE TABLE `users` (
   `created_at` datetime DEFAULT NULL,
   `last_login_at` datetime DEFAULT NULL,
   `auth_key` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -238,27 +240,6 @@ LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` VALUES (1,'admin','admin','admin@admin.com','$2y$13$c/a//d76.nbnlpst.Pmgkeqf9zYyJRpgy8T6PfEv0g02Tbz/DpJGS',NULL,'active','2016-11-23 13:56:58','2016-11-23 13:56:58','uGi-pBE8U8N7dqYa01QFb0TlzaEBRkZk');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-
-DROP TABLE IF EXISTS `mail_template`;
-
-CREATE TABLE `mail_template` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `key` varchar(255) DEFAULT NULL,
-  `body` text,
-  `name` varchar(255) NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  `subject` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `key` (`key`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
-/*Data for the table `mail_template` */
-
-LOCK TABLES `mail_template` WRITE;
-
-insert  into `mail_template`(`id`,`key`,`body`,`name`,`updated_at`,`subject`) values (1,'REGISTER','<p>Hello {{user}}</p>\r\n\r\n\r\n\r\n<p>In {{data}}</p>\r\n\r\n\r\n\r\n<p>Visit link {{link}}</p>\r\n\r\n\r\n\r\n<p>Restore password {{password}}</p>\r\n\r\n\r\n\r\n<p>Second password {{password2}}</p>\r\n\r\n\r\n\r\n<p>{{wrong}}</p>\r\n\r\n\r\n\r\n<p>&nbsp;</p>\r\n\r\n','Register','2016-12-22 17:16:52','Test regisret user');
-
 UNLOCK TABLES;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
