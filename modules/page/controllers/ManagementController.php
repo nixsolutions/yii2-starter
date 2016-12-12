87a9cf74a836c2d2b791f12428b619d8a2663735
Elizaveta Zmievskaya <zmievskaya.liza@mail.ru>
2016-12-12 18:17:15 +0200

    Action Update changed

--- modules/page/controllers/ManagementController.php
+++ modules/page/controllers/ManagementController.php
@@ -73,11 +73,10 @@ class ManagementController extends Controller
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
-        } else {
-            return $this->render('update', [
-                'model' => $model,
-            ]);
         }
+        return $this->render('update', [
+            'model' => $model,
+        ]);
     }
 
     /**
