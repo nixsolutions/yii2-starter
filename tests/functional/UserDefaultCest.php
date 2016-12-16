c3f3a31aa948e0370b400ca72de485a2be6fa641
Artem Tarasov <tema.tarasow@gmail.com>
2016-12-16 12:16:07 +0200

    Tests: create base class

--- tests/functional/UserDefaultCest.php
+++ tests/functional/UserDefaultCest.php
@@ -1,30 +1,10 @@
 <?php
 
+use app\tests\functional\BaseFunctionalCest;
 use yii\helpers\Url;
 
-class UserDefaultCest
+class UserDefaultCest extends BaseFunctionalCest
 {
-    protected function loginAsAdmin(FunctionalTester $I)
-    {
-        $I->amOnRoute('/login');
-        $I->seeInTitle('Login');
-        $I->amLoggedInAs(1);
-    }
-
-    protected function loginAsUser(FunctionalTester $I)
-    {
-        $I->amOnRoute('/login');
-        $I->seeInTitle('Login');
-        $I->amLoggedInAs(2);
-    }
-
-    protected function logout(FunctionalTester $I)
-    {
-        $I->click('.btn.btn-link.logout');
-        $I->amOnPage('/');
-        $I->see('Login', 'a');
-    }
-
     /**
      * @before loginAsAdmin
      * @after logout
