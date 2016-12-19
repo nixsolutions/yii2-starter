6589bbb6762dc010f18b24ad51af03fafeae76e7
Artem Tarasov <artem.tarasov@nixsolutions.com>
2016-12-19 18:36:36 +0200

    Update pipeline

---
+++ docker_install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+service mysql start
+mysql -h localhost --user=root --password=root -e "CREATE DATABASE IF NOT EXISTS yii_starter_tests CHARACTER SET utf8 COLLATE utf8_general_ci;"
+composer global require "fxp/composer-asset-plugin:^1.2.1"
+composer install
+cp config/params.php.sample config/params.php
+cp config/db.php.sample config/db.php
+cp config/mailer.php.sample config/mailer.php
