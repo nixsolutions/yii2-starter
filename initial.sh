23525d53717fb0be6ea74725badb216b2d77cce1
Artem Tarasov <artem.tarasov@nixsolutions.com>
2016-12-20 10:28:22 +0000

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 service mysql start
 mysql -h localhost --user=root --password=root -e "CREATE DATABASE IF NOT EXISTS yii_starter_tests CHARACTER SET utf8 COLLATE utf8_general_ci;"
+mysql -h localhost --user=root --password=root yii_starter_tests < tests/_data/dump.sql
 composer global require "fxp/composer-asset-plugin:^1.2.1"
 composer install
 cp config/params.php.sample config/params.php
