7679966486fb8fe62966515491e3b85d0646f20d
Artem Tarasov <artem.tarasov@nixsolutions.com>
2017-01-13 12:50:23 +0200

    Run build-in php server for test crop avatar via REST module

--- initial.sh
+++ initial.sh
@@ -8,3 +8,4 @@ cp config/params.php.sample config/params.php
 cp config/db.php.sample config/db.php
 cp config/mailer.php.sample config/mailer.php
 cp config/clients.php.sample config/clients.php
+php yii serve --port=8888
