1b074d8d9c945bae0e125be17940383f6f5c6763
Elizaveta Zmievskaya <zmievskaya.liza@mail.ru>
2017-01-27 12:41:21 +0200

    Change hash type

--- modules/user/models/Hash.php
+++ modules/user/models/Hash.php
@@ -18,7 +18,7 @@ class Hash extends ActiveRecord
 {
     const TYPE_REGISTER = 'register';
     const TYPE_RECOVER = 'recover';
-    const TYPE_CHANGE_PASSWORD = 'change-password';
+    const TYPE_CHANGE_PASSWORD = 'change password';
 
     /**
      * @inheritdoc
